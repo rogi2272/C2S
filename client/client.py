8cf8728dcc4ea76d3ce947beb15875b0f1e4eecb
rogi2272 <rodrigosg.rodrigosg@gmail.com>
2025-07-16 18:57:12 -0300

    Add files via upload

---
+++ client/client.py
@@ -0,0 +1,8 @@
+# ============================
+# client/client.py
+# ============================
+
+def send_filters(filtros):
+    from server.server import handle_request
+    return handle_request(filtros)
+
